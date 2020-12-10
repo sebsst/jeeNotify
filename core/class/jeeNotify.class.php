bede36ee5436cde79e24dd85899e8b26d10421bd
sebsst <sebastien.siegrist@yahoo.fr>
2020-12-10 19:31:40 +0100

    Update jeeNotify.class.php

--- core/class/jeeNotify.class.php
+++ core/class/jeeNotify.class.php
@@ -20,14 +20,112 @@
 require_once __DIR__  . '/../../../../core/php/core.inc.php';
 
 class template extends eqLogic {
+
+
+    public static function dependancy_info() {
+      $return = array();
+      $return['log'] = 'jeeNotify_install';
+      $mailparser = realpath(dirname(__FILE__) . '/../../resources/node_modules/jeedNotify');
+      $return['progress_file'] = '/tmp/jeeNotify_dep';
+      if (is_dir($mailparser)) {
+        $return['state'] = 'ok';
+      } else {
+        $return['state'] = 'nok';
+      }
+      return $return;
+    }
+
+    public static function dependancy_install() {
+      $install_path = dirname(__FILE__) . '/../../resources';
+      //passthru('/bin/bash ' . $install_path . '/nodejs.sh ' . $install_path . ' jeeNotify >> ' . log::getPathToLog('jeeNotify_install') . ' 2>&1 &');
+    }
+
+    public static function deamon_start($_debug = false) {
+      self::deamon_stop();
+      $deamon_info = self::deamon_info();
+     $fd = inotify_init();
+     if ($deamon_info['launchable'] != 'ok') {
+        throw new Exception(__('Veuillez vérifier la configuration', __FILE__));
+      }
+      log::add('jeeNotify', 'info', 'Lancement du démon jeeNotify');
+      $watch_descriptor = inotify_add_watch($fd, $file, IN_ALL_EVENTS);
+      //$url = network::getNetworkAccess('internal') . '/plugins/jeeNotify/core/api/jeeNotify.php?apikey=' . jeedom::getApiKey('jeeNotify');
+
+      $service_path = realpath(dirname(__FILE__) . '/../../resources');
+      $attach_path = $service_path . '/attachments/';
+
+      foreach ($deamon_info['notlaunched'] as $addr) {
+        $mail = self::byLogicalId($addr, 'jeeNotify');
+        $cmd = 'nice -n 19 nodejs ' . $service_path . '/jeeNotify.js ' . $addr . ' ' . $url . ' "' . $mail->getConfiguration('username') . '" "' . $mail->getConfiguration('password') . '" ' . $mail->getConfiguration('server') . ' ' . $mail->getConfiguration('port') . ' ' . $mail->getConfiguration('attach') . ' ' . $attach_path;
+
+        log::add('jeeNotify', 'debug', 'Lancement démon jeeNotify : ' . $cmd);
+        $result = exec('nohup ' . $cmd . ' >> ' . log::getPathToLog('jeeNotify_node') . ' 2>&1 &');
+        if (strpos(strtolower($result), 'error') !== false || strpos(strtolower($result), 'traceback') !== false) {
+          log::add('jeeNotify', 'error', $result);
+          return false;
+        }
+      }
+      $i = 0;
+      while ($i < 30) {
+        $deamon_info = self::deamon_info();
+        if ($deamon_info['state'] == 'ok') {
+          break;
+        }
+        sleep(1);
+        $i++;
+      }
+      if ($i >= 30) {
+        log::add('jeeNotify', 'error', 'Impossible de lancer un démon jeeNotify, vérifiez le port', 'unableStartDeamon');
+        return false;
+      }
+      message::removeAll('jeeNotify', 'unableStartDeamon');
+      log::add('jeeNotify', 'info', 'Démons jeeNotify lancé');
+      return true;
+    }
+
+    public static function deamon_info() {
+      $return = array();
+      $return['log'] = 'jeeNotify';
+      $return['state'] = 'ok';
+      $return['launchable'] = 'ok';
+      $return['notlaunched'] = array();
+      $return['launched'] = array();
+      foreach (eqLogic::byType('jeeNotify') as $jeeNotify) {
+          if ($jeeNotify->getIsEnable() == 1 ) {
+            $pid = trim( shell_exec ('ps ax | grep "jeeNotify/resources/jeeNotify.js '. $jeeNotify->getConfiguration('addr') . '" | grep -v "grep" | wc -l') );
+            if ($pid != '' && $pid != '0') {
+              $return['launched'][] = $jeeNotify->getConfiguration('addr');
+            } else {
+              $return['state'] = 'nok';
+              $return['notlaunched'][] = $jeeNotify->getConfiguration('addr');
+              $return['launchable_message'] = $jeeNotify->getConfiguration('addr') . ' non lancé';
+            }
+            if ($jeeNotify->getConfiguration('addr') == '') {
+              $return['launchable'] = 'nok';
+              $return['launchable_message'] = __('Le port de ' . $jeeNotify->getName() . ' n\'est pas configuré', __FILE__);
+            }
+          }
+      }
+      return $return;
+    }
+
+    public static function deamon_stop() {
+
+      inotify_rm_watch($fd, $watch_descriptor);
+      log::add('jeeNotify', 'info', 'Arrêt du service jeeNotify');
+      $deamon_info = self::deamon_info();
+      if (count($deamon_info['launched']) != 0) {
+      }
+    }
+
     /*     * *************************Attributs****************************** */
-    
+
   /*
    * Permet de définir les possibilités de personnalisation du widget (en cas d'utilisation de la fonction 'toHtml' par exemple)
    * Tableau multidimensionnel - exemple: array('custom' => true, 'custom::layout' => false)
 	public static $_widgetPossibility = array();
    */
-    
+
     /*     * ***********************Methode static*************************** */
 
     /*
@@ -47,19 +145,19 @@ class template extends eqLogic {
       public static function cron10() {
       }
      */
-    
+
     /*
      * Fonction exécutée automatiquement toutes les 15 minutes par Jeedom
       public static function cron15() {
       }
      */
-    
+
     /*
      * Fonction exécutée automatiquement toutes les 30 minutes par Jeedom
       public static function cron30() {
       }
      */
-    
+
     /*
      * Fonction exécutée automatiquement toutes les heures par Jeedom
       public static function cronHourly() {
@@ -75,45 +173,45 @@ class template extends eqLogic {
 
 
     /*     * *********************Méthodes d'instance************************* */
-    
- // Fonction exécutée automatiquement avant la création de l'équipement 
+
+ // Fonction exécutée automatiquement avant la création de l'équipement
     public function preInsert() {
-        
+
     }
 
- // Fonction exécutée automatiquement après la création de l'équipement 
+ // Fonction exécutée automatiquement après la création de l'équipement
     public function postInsert() {
-        
+
     }
 
- // Fonction exécutée automatiquement avant la mise à jour de l'équipement 
+ // Fonction exécutée automatiquement avant la mise à jour de l'équipement
     public function preUpdate() {
-        
+
     }
 
- // Fonction exécutée automatiquement après la mise à jour de l'équipement 
+ // Fonction exécutée automatiquement après la mise à jour de l'équipement
     public function postUpdate() {
-        
+
     }
 
- // Fonction exécutée automatiquement avant la sauvegarde (création ou mise à jour) de l'équipement 
+ // Fonction exécutée automatiquement avant la sauvegarde (création ou mise à jour) de l'équipement
     public function preSave() {
-        
+
     }
 
- // Fonction exécutée automatiquement après la sauvegarde (création ou mise à jour) de l'équipement 
+ // Fonction exécutée automatiquement après la sauvegarde (création ou mise à jour) de l'équipement
     public function postSave() {
-        
+
     }
 
- // Fonction exécutée automatiquement avant la suppression de l'équipement 
+ // Fonction exécutée automatiquement avant la suppression de l'équipement
     public function preRemove() {
-        
+
     }
 
- // Fonction exécutée automatiquement après la suppression de l'équipement 
+ // Fonction exécutée automatiquement après la suppression de l'équipement
     public function postRemove() {
-        
+
     }
 
     /*
@@ -140,11 +238,11 @@ class template extends eqLogic {
 
 class templateCmd extends cmd {
     /*     * *************************Attributs****************************** */
-    
+
     /*
       public static $_widgetPossibility = array();
     */
-    
+
     /*     * ***********************Methode static*************************** */
 
 
@@ -157,12 +255,10 @@ class templateCmd extends cmd {
       }
      */
 
-  // Exécution d'une commande  
+  // Exécution d'une commande
      public function execute($_options = array()) {
-        
+
      }
 
     /*     * **********************Getteur Setteur*************************** */
 }
-
-
